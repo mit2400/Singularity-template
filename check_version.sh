58bd5970f0da4ce5569755eb03dbf30a9104f577
minkoo <mit2400@naver.com>
2023-04-12 15:07:54 +0900

    torch, cuda, cudnn install version check script

---
+++ check_version.sh
@@ -0,0 +1,4 @@
+import torch 
+print("Torch version:{}".format(torch.__version__))
+print("cuda version: {}".format(torch.version.cuda))
+print("cudnn version:{}".format(torch.backends.cudnn.version()))
